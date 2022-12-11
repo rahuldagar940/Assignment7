5148694e8094d84cecbaf685de04da9dec8b87ea
Rahul Dagar <49279174+rahuldagar940@users.noreply.github.com>
2022-12-11 18:20:56 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA8shZMiDrAbbg8VpICthYHn7JxPW2CGNk",
-    authDomain: "photoapplication22.firebaseapp.com",
-    projectId: "photoapplication22",
-    storageBucket: "photoapplication22.appspot.com",
-    messagingSenderId: "967857986538",
-    appId: "1:967857986538:web:35253bbb6d00f47b243093",
+    apiKey: "",
+    authDomain: ".firebaseapp.com",
+    projectId: "",
+    storageBucket: ".appspot.com",
+    messagingSenderId: "",
+    appId: "1::web:",
   },
   API_URL: "http://3.131.157.38:8080/api/"
   
